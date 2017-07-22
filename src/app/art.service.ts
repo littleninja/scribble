4334d0c7fd0a9798074191d1a2e47a699b8248c1
Sarah Grefalda <sarahrhgrefalda@gmail.com>
2017-07-21 23:50:25 -0700

    Add art from storage

--- src/app/art.service.ts
+++ src/app/art.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-import * as firebase from 'firebase/app';
+import * as firebase from 'firebase';
+import { FirebaseApp } from 'angularfire2';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class ArtService {
 
-  constructor(private _db: AngularFireDatabase) { }
+  private _storageRef;
+
+  constructor(private _db: AngularFireDatabase, firebaseApp: FirebaseApp) {
+    const bucket = `gs://${environment.firebase.storageBucket}/art`;
+    this._storageRef = firebase.storage(firebaseApp).refFromURL(bucket);
+  }
 
   getArt(): Observable<any> {
     return this._db.list('/art').map((art) => {
-      return art;
+      return art.map(a => {
+        a.storagePromise = this._storageRef.child(`${a.fileName}`).getDownloadURL();
+        return a;
+      })
     });
   }
 
